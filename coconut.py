11cf25de3a453f6c1127912aca69da1b6c931593
uky-syauki <achmad.uky@gmail.com>
2023-10-04 17:02:26 +0800

    update v.1.2

--- coconut.py
+++ coconut.py
@@ -1,12 +1,4 @@
-from flask import Flask
-
-app = Flask(__name__)
-
-@app.route('/')
-def index():
-	return '<h1>Bismillah</h1>'
-
-
+from app import app
 
 if __name__=='__main__':
 	app.run(host='0.0.0.0', debug=True)
