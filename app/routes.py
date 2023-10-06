ad6a81e0432f81421ef3d0110a32cf132ed4a2d7
uky-syauki <achmad.uky@gmail.com>
2023-10-06 17:22:38 +0800

    3 local

--- app/routes.py
+++ app/routes.py
@@ -6,7 +6,7 @@ from app import app
 
 @app.route('/')
 def index():
-    return render_template('base.html')
+    return render_template('index.html')
 
 
 @app.route('/laporan', methods=['POST','GET'])
